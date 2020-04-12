f174145e6ba7eaca011f5d5627d4c193218d9a1d
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-12 14:38:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,22 @@ def do_admin_login():
     return homereg()
 
 
+@app.route('/test')
+def test():
+
+    POST_USERNAME = "python"
+    POST_PASSWORD = "python"
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
+
+
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
