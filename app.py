8e35b766d8c6a21770d732a8826fdb7080c278fb
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-12 15:45:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 app = Flask(__name__)
 engine = create_engine('sqlite:///tutorial.db', echo=True)
 app.secret_key = os.urandom(12).hex()
+application.config['SQLALCHEMY_DATABASE_URI'] = postgresURI
 
 
 @app.route("/")
